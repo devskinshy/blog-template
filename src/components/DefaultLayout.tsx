94aef1bff3bd44805070f95844f35437e790898a
devskinshy <yyshgk12@gmail.com>
2024-09-16 21:23:49 +0900

    project setting

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -6,10 +6,28 @@ function DefaultLayout() {
 			<header className='flex flex-row'>Header</header>
 			<div className='flex flex-1 flex-col overflow-hidden'>
 				<div className='flex flex-1 flex-col overflow-hidden sm:flex-row'>
+					<nav className='max-h-10 max-w-full flex-initial overflow-scroll border sm:max-h-full sm:max-w-40'>
+						<div>
+							<div style={{ width: 10000, height: 10000 }}>LeftSideBar</div>
+							LeftSideBar
+						</div>
+					</nav>
 					<main className='flex-1 overflow-scroll border'>
 						<div style={{ width: 10000, height: 10000 }}>main</div>
 					</main>
+					<aside className='max-h-10 max-w-full flex-initial overflow-scroll border sm:max-h-full sm:max-w-40'>
+						<div>
+							<div style={{ width: 10000, height: 10000 }}>RightSideBar</div>
+							RightSideBar
+						</div>
+					</aside>
 				</div>
+				<article className='max-h-10 max-w-full flex-initial overflow-scroll border'>
+					<div>
+						<div style={{ width: 10000, height: 10000 }}>terminal</div>
+						terminal
+					</div>
+				</article>
 			</div>
 			<footer className='flex flex-row'>Footer</footer>
 		</div>
