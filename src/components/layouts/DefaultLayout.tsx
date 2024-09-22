42eacb0a24599baf701d8831f0bf4337612edc1d
devskinshy <yyshgk12@gmail.com>
2024-09-22 23:42:57 +0900

    todo resizable

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -4,6 +4,11 @@ import DefaultFooter from '@/components/layouts/items/DefaultFooter';
 import DefaultNav from '@/components/layouts/items/DefaultNav';
 import DefaultAside from '@/components/layouts/items/DefaultAside';
 import DefaultTerminal from '@/components/layouts/items/DefaultTerminal';
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from '@/components/ui/resizable';
 
 function DefaultLayout({
 	children,
@@ -13,17 +18,22 @@ function DefaultLayout({
 	return (
 		<div className='flex h-screen w-screen flex-col'>
 			<DefaultHeader />
-			<div className='flex flex-1 flex-col overflow-hidden'>
-				<div className='flex flex-1 flex-row overflow-hidden'>
+			<ResizablePanelGroup
+				direction='vertical'
+				className='flex flex-1 flex-col overflow-hidden'>
+				<ResizablePanel className='flex flex-1 flex-row overflow-hidden'>
 					<DefaultNav />
 					<main className='flex-1 overflow-scroll border'>
 						{/* <div style={{ width: 10000, height: 10000 }}>{children}</div> */}
 						{children}
 					</main>
 					<DefaultAside />
-				</div>
-				<DefaultTerminal />
-			</div>
+				</ResizablePanel>
+				<ResizableHandle />
+				<ResizablePanel>
+					<DefaultTerminal />
+				</ResizablePanel>
+			</ResizablePanelGroup>
 			<DefaultFooter />
 		</div>
 	);
