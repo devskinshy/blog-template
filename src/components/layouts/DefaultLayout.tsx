c7db51e6f1d3cb74c49ad40fda8efaa56d82882f
devskinshy <yyshgk12@gmail.com>
2024-10-01 00:01:02 +0900

    project setting

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -16,48 +16,45 @@ function DefaultLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<div className='grid h-screen w-screen grid-cols-[auto_1fr_auto] grid-rows-[auto_1fr_auto]'>
+		<div className='flex h-screen w-screen flex-col'>
 			<DefaultHeader />
-			<nav className='border-r-[1px]'>nav</nav>
-			<main>
-				<ResizablePanelGroup direction='vertical'>
-					<ResizablePanel defaultSize={75}>
-						<ResizablePanelGroup direction='horizontal'>
-							<ResizablePanel
-								collapsible
-								collapsedSize={0}
-								defaultSize={0}
-								minSize={10}
-								maxSize={50}>
-								<nav>nav</nav>
-							</ResizablePanel>
-							<ResizableHandle />
-							<ResizablePanel>
-								<main>main</main>
-							</ResizablePanel>
-							<ResizableHandle />
-							<ResizablePanel
-								collapsible
-								collapsedSize={0}
-								defaultSize={0}
-								minSize={10}
-								maxSize={50}>
-								<aside>aside</aside>
-							</ResizablePanel>
-						</ResizablePanelGroup>
-					</ResizablePanel>
-					<ResizableHandle />
-					<ResizablePanel
-						collapsible
-						collapsedSize={0}
-						defaultSize={25}
-						minSize={10}
-						maxSize={50}>
-						<article>terminal</article>
-					</ResizablePanel>
-				</ResizablePanelGroup>
-			</main>
-			<aside className='border-l-[1px]'>aside</aside>
+			<ResizablePanelGroup direction='horizontal'>
+				<ResizablePanel
+					collapsible
+					collapsedSize={5}
+					defaultSize={5}
+					minSize={10}
+					maxSize={50}>
+					<nav>nav</nav>
+				</ResizablePanel>
+				<ResizableHandle />
+				<ResizablePanel defaultSize={90}>
+					{/* <ResizablePanelGroup direction='vertical'> */}
+					{/*	<ResizablePanel defaultSize={100}> */}
+					{/*		<main>main</main> */}
+					{/*	</ResizablePanel> */}
+					{/*	<ResizableHandle /> */}
+					{/*	<ResizablePanel */}
+					{/*		collapsible */}
+					{/*		collapsedSize={0} */}
+					{/*		defaultSize={0} */}
+					{/*		minSize={10} */}
+					{/*		maxSize={50}> */}
+					{/*		<article>terminal</article> */}
+					{/*	</ResizablePanel> */}
+					{/* </ResizablePanelGroup> */}
+					<main>main</main>
+				</ResizablePanel>
+				<ResizableHandle />
+				<ResizablePanel
+					collapsible
+					collapsedSize={5}
+					defaultSize={5}
+					minSize={10}
+					maxSize={50}>
+					<aside>aside</aside>
+				</ResizablePanel>
+			</ResizablePanelGroup>
 			<DefaultFooter />
 		</div>
 	);
