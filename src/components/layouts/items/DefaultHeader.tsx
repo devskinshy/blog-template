96006132cae36fd5fd8c65ab5430861c68b214a1
devskinshy <yyshgk12@gmail.com>
2024-10-07 23:10:46 +0900

    project setting

--- src/components/layouts/items/DefaultHeader.tsx
+++ src/components/layouts/items/DefaultHeader.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {
+	Menubar,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarSeparator,
+	MenubarShortcut,
+	MenubarTrigger,
+} from '@/components/ui/menubar';
 
 function DefaultHeader() {
 	return (
-		<header className='border-b-[1px]'>
-			<div className='flex justify-center align-middle'>header</div>
+		<header>
+			<Menubar>
+				<MenubarMenu>
+					<MenubarTrigger>File</MenubarTrigger>
+					<MenubarContent>
+						<MenubarItem>
+							New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+						</MenubarItem>
+						<MenubarItem>New Window</MenubarItem>
+						<MenubarSeparator />
+						<MenubarItem>Share</MenubarItem>
+						<MenubarSeparator />
+						<MenubarItem>Print</MenubarItem>
+					</MenubarContent>
+				</MenubarMenu>
+			</Menubar>
 		</header>
 	);
 }
