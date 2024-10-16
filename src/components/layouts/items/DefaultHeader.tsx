2e1098bf44608bd891132cd53c5a847383fd581a
devskinshy <yyshgk12@gmail.com>
2024-10-16 23:29:43 +0900

    project setting

--- src/components/layouts/items/DefaultHeader.tsx
+++ src/components/layouts/items/DefaultHeader.tsx
@@ -1,33 +1,20 @@
 import React from 'react';
 
-import {
-	Menubar,
-	MenubarContent,
-	MenubarItem,
-	MenubarMenu,
-	MenubarSeparator,
-	MenubarShortcut,
-	MenubarTrigger,
-} from '@/components/ui/menubar';
+import { Button } from '@/components/ui/button';
 
 function DefaultHeader() {
 	return (
-		<header>
-			<Menubar>
-				<MenubarMenu>
-					<MenubarTrigger>File</MenubarTrigger>
-					<MenubarContent>
-						<MenubarItem>
-							New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-						</MenubarItem>
-						<MenubarItem>New Window</MenubarItem>
-						<MenubarSeparator />
-						<MenubarItem>Share</MenubarItem>
-						<MenubarSeparator />
-						<MenubarItem>Print</MenubarItem>
-					</MenubarContent>
-				</MenubarMenu>
-			</Menubar>
+		<header className='flex justify-center border-b-[1px] p-1 align-middle'>
+			<Button
+				variant='outline'
+				className='relative h-8 w-96 justify-center rounded-[0.5rem] bg-muted/50 align-middle text-sm font-normal text-muted-foreground shadow-none'
+			>
+				<span className='hidden lg:inline-flex'>Search documentation...</span>
+				<span className='inline-flex lg:hidden'>Search...</span>
+				<kbd className='pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex'>
+					<span className='text-xs'>⌘</span>K
+				</kbd>
+			</Button>
 		</header>
 	);
 }
